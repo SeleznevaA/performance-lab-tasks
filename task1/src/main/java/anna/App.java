948ed24bad9248aa730b1e7234a933a0dd3c2bf3
Selezneva Anna <selezneva_anna@icloud.com>
2019-04-24 22:08:03 +0300

    init

---
+++ task1/src/main/java/anna/App.java
@@ -0,0 +1,22 @@
+package anna;
+
+//import com.google.common.collect.ImmutableSet;
+//import com.google.common.collect.Sets;
+//
+//import static com.google.common.collect.Sets.intersection;
+
+public class App {
+    public static void main(String[] args) {
+
+//        ImmutableSet<Integer> set1 = ImmutableSet.of(10, 20, 30);
+//        ImmutableSet<Object> set2 = ImmutableSet.builder()
+//                .add(20)
+//                .add(30)
+//                .add(40)
+//                .build();
+//
+//        System.out.println(intersection(set1, set2));
+
+        System.out.println("HELLO!");
+    }
+}
