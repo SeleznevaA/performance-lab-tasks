1697648ad9358761af44f063e86e3895e73906f8
Selezneva Anna <selezneva_anna@icloud.com>
2019-04-25 12:00:50 +0300

    task3 - add comments

--- task3/src/main/java/anna/App.java
+++ task3/src/main/java/anna/App.java
@@ -3,46 +3,36 @@ package anna;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class App {
 
     public static void main(String[] args) {
-        CashierMeasures measures1;
-        CashierMeasures measures2;
-        CashierMeasures measures3;
-        CashierMeasures measures4;
-        CashierMeasures measures5;
-        try {
-            measures1 = new CashierMeasures(readNumbers(args[0]));
-            measures2 = new CashierMeasures(readNumbers(args[1]));
-            measures3 = new CashierMeasures(readNumbers(args[2]));
-            measures4 = new CashierMeasures(readNumbers(args[3]));
-            measures5 = new CashierMeasures(readNumbers(args[4]));
-        } catch (IOException e) {
-            throw new IllegalArgumentException(e);
-        }
-
-        List<CashierMeasures> measuresOfAllCashier = new ArrayList<>();
-        measuresOfAllCashier.add(measures1);
-        measuresOfAllCashier.add(measures2);
-        measuresOfAllCashier.add(measures3);
-        measuresOfAllCashier.add(measures4);
-        measuresOfAllCashier.add(measures5);
+        List<CashierMeasures> measuresOfAllCashier = IntStream.rangeClosed(0, 4)
+                .mapToObj(i -> args[i])
+                .map(App::readNumbers)
+                .map(CashierMeasures::new)
+                .collect(toList());
 
         CustomerCountAnalyzer customerCountAnalyzer = new CustomerCountAnalyzer(measuresOfAllCashier);
 
         int halfHour = customerCountAnalyzer.getIntensiveNumberOfHalfHour();
 
-        System.out.println(halfHour + "'th half hour of work");
+        // с момента откртытия на halfHour получасовом интервале наблюдалось наибольшее количество посетителей
+        System.out.println(halfHour + "'th half hour from the beginning of work");
     }
 
-    private static List<Double> readNumbers(String fileName) throws IOException {
-        return Files.readAllLines(Paths.get(fileName))
-                .stream()
-                .map(Double::parseDouble)
-                .collect(Collectors.toList());
+    private static List<Double> readNumbers(String fileName) {
+        try {
+            return Files.readAllLines(Paths.get(fileName))
+                    .stream()
+                    .map(Double::parseDouble)
+                    .collect(toList());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
